25ad2d4f0e499e10f806411f326eca17795046b9
Abduvahob <dilmurodayitboyev@gmail.com>
2022-11-26 14:48:39 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let son = +prompt("son kiriting  ");
+for (let i = 1; i <= son; i++) {
+  if (i % 2 === 0) {
+    console.log(`${i}`);
+  }
+}
+
+//let = +prompt(` xoxlagan soningizni kiriting`);
+
+let summa = 0;
+
+for (let i = 0; i <= son; i++) {
+  summa = summa + i;
+}
+alert(summa);
